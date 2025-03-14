63ff54a8f1cabcf7da5b21a5e2f6e873195151ff
nosfcj <seuemail@example.com>
2025-03-14 07:42:19 -0500

    Exclusão de migrations

--- api/src/models/Servico.ts
+++ api/src/models/Servico.ts
@@ -2,7 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, JoinColum
 import { Orgao } from './Orgao';
 import { Conteudo } from './Conteudo';
 import { Direcionamento } from './Direcionamento';
-import { Acao } from './Acao';
+import { Acao } from './Acao'; // Adicione esta importação
 
 @Entity({ name: 'Servico' })
 export class Servico {
@@ -19,24 +19,30 @@ export class Servico {
   @Column({
     length: 255,
     nullable: false,
-    comment: 'Título do serviço',
+    comment: 'Título do serviço disponibilizado',
   })
   titulo!: string;
 
-  // Relação com Orgao
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    comment: 'Última atualização relevante para o cache de serviços (mudanças de status, conteúdo, requisitos)',
+    name: 'ultimaAtualizacao'
+  })
+  ultimaAtualizacao!: Date;
+
+  // Adicione esta nova relação com Acao
+  @OneToMany(() => Acao, (acao) => acao.servico)
+  acoes!: Acao[];
+
   @ManyToOne(() => Orgao, (orgao) => orgao.servicos)
   @JoinColumn({ name: 'Orgao_ID' })
   orgao!: Orgao;
 
-  // Relação com Conteudo
   @OneToMany(() => Conteudo, (conteudo) => conteudo.servico)
   conteudos!: Conteudo[];
 
-  // Relação com Direcionamento (adicionada)
   @OneToMany(() => Direcionamento, (direcionamento) => direcionamento.servico)
   direcionamentos!: Direcionamento[];
-
-  @OneToMany(() => Acao, (acao) => acao.servico)
-  acoes!: Acao[];
-
 }
